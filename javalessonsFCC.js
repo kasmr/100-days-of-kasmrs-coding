be1b29fb8424eb2d19d02f24924226830e89edea
kasmr <alikkras@gmail.com>
2019-03-18 19:41:03 +0300

    day started javascript

---
+++ javalessonsFCC.js
@@ -0,0 +1,23 @@
+//*Today at the day #2 i've been studying java script on freecodecamp.com! Just on the basics right now, let's go not to just basics!:)//
+
+//Concatenating Strings with Plus Operator//
+
+var myStr = 'This is the start.' + ' This is the end.';
+
+//Concatenating Strings with the Plus Equals Operator//
+
+var myStr = 'This is the first sentence. ';
+myStr += 'This is the second sentence.';
+
+//Constructing Strings with Variables//
+
+var myName = "Alik";
+var myStr = "My name is" + myName + " and I am well!";
+
+//Appending Variables to Strings//
+
+var someAdjective = "fun!";
+var myStr = "Learning to code is ";
+myStr += someAdjective; 
+
+//that was it for that day 2//
