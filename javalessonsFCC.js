35dfb43372005c73230eccbf78f660b34b389ea4
kasmr <alikkras@gmail.com>
2019-03-23 18:30:14 +0300

    first part of a day 4

--- javalessonsFCC.js
+++ javalessonsFCC.js
@@ -75,3 +75,50 @@ function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
   
   // Change the words here to test your function
   wordBlanks("dog", "big", "ran", "quickly");
+
+  // Day 4 here lets get started! :)
+  //Basic JavaScript: Store Multiple Values in one Variable using JavaScript Arrays
+
+  var myArray = ["Alik", 23];
+
+  //Basic JavaScript: Nest one Array within Another Array
+
+  var myArray = [["i love sopranos", 1], ["but the best season is", 2]];
+
+  //Basic JavaScript: Access Array Data with Indexes
+
+  // Setup
+var myArray = [50,60,70];
+var myData = myArray[0];
+
+//Basic JavaScript: Modify Array Data With Indexes
+
+// Setup
+var myArray = [18,64,99];
+myArray[0] = 45;
+
+//Basic JavaScript: Access Multi-Dimensional Arrays With Indexes
+//Using bracket notation select an element from myArray such that myData is equal to 8.
+ // Setup
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+// Only change code below this line.
+var myData = myArray[2][1];
+
+//Basic JavaScript: Manipulate Arrays With push()
+//Push ["dog", 3] onto the end of the myArray variable.
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog",3]);
+
+//Basic JavaScript: Manipulate Arrays With pop()
+//Use the .pop() function to remove the last item from myArray, assigning the "popped off" value to removedFromMyArray
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.pop();
