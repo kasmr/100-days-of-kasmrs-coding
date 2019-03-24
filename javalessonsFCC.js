89839af979944d8ca8ee14f34485f2ecdb54cb2e
Alik <alikkras@gmail.com>
2019-03-24 13:33:13 +0300

    first part of a day 5

--- javalessonsFCC.js
+++ javalessonsFCC.js
@@ -122,3 +122,41 @@ var myArray = [["John", 23], ["cat", 2]];
 
 // Only change code below this line.
 var removedFromMyArray = myArray.pop();
+
+// Day 5 here lets go!
+//Basic JavaScript: Manipulate Arrays With shift()
+//Use the .shift() function to remove the first item from myArray, assigning the "shifted off" value to removedFromMyArray.
+ 
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line.
+var removedFromMyArray = myArray.shift();
+
+//Basic JavaScript: Manipulate Arrays With unshift()
+//Add ["Paul",35] to the beginning of the myArray variable using unshift().
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+myArray.unshift(["Paul", 35])
+
+//Basic JavaScript: Shopping List
+//The first element in each sub-array should contain a string with the name of the item. The second element should be a number representing the quantity i.e.
+
+var myList = [["onion", 1], ["bread", 1], ["tomatos", 10], ["cucambers", 5], ["sausages", 2]];
+
+//Basic JavaScript: Write Reusable JavaScript with Functions
+//Create a function called reusableFunction which prints "Hi World" to the dev console.
+//Call the function.
+
+function reusableFunction() {console.log("Hi World");}
+reusableFunction();
+
+//Basic JavaScript: Passing Values to Functions with Arguments
+//Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+//Call the function with two numbers as arguments.
+
+function functionWithArgs(a, b) {console.log(a+b);}
+functionWithArgs(1, 2);
+functionWithArgs(7, 9);
