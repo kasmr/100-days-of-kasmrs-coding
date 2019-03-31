6878396ed68a5b438d2b29d9718ff2d53076b281
kasmr <alikkras@gmail.com>
2019-03-31 11:03:05 +0300

    dat 10 started

--- javascript-lessons-FCC.js
+++ javascript-lessons-FCC.js
@@ -749,3 +749,16 @@ switch (val) {
 
 // Change this value to test
 chainToSwitch(7);
+
+//Day 10!
+
+//Basic JavaScript: Returning Boolean Values from Functions
+//Fix the function isLess to remove the if/else statements.
+
+function isLess(a, b) {
+  // Fix this code
+  return a <= b; 
+  }
+
+// Change these values to test
+isLess(10, 15);
