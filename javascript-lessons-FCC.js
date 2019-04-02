006521a1da0eccb05834b7f7fb64572e0f3e2a5b
kasmr <alikkras@gmail.com>
2019-04-02 23:28:15 +0300

    day 10

--- javascript-lessons-FCC.js
+++ javascript-lessons-FCC.js
@@ -762,3 +762,33 @@ function isLess(a, b) {
 
 // Change these values to test
 isLess(10, 15);
+
+//Day 11! 
+
+/*Basic JavaScript: Counting Cards
+You will write a card counting function. It will receive a card parameter, which can be a number or a string, and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string Bet if the count is positive, or Hold if the count is zero or negative. The current count and the player's decision (Bet or Hold) should be separated by a single space.
+
+Example Output
+-3 Hold
+5 Bet
+
+Hint
+Do NOT reset count to 0 when value is 7, 8, or 9.
+Do NOT return an array.
+Do NOT include quotes (single or double) in the output.*/
+
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  
+  
+  return "Change Me";
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+//got really stuck wiht this
