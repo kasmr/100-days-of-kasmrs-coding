08c2130659aeb65caff4674136600f4b5f7ece3e
Alik <alikkras@gmail.com>
2019-09-18 19:30:17 +0300

    Day 38

---
+++ Java-Script-Projects/NetflixLandingPage/js/main.js
@@ -0,0 +1,10 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContentItems = document.querySelectorAll(".tab-content-item");
+
+//Select tab content item
+function selectItem(e) {
+  //Add border tc current tab
+}
+
+//Listen for tab click
+tabItems.forEach(item => item.addEventListener("click", selectItem));
